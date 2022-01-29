4d020833c9ab4036f70b047d9a421a7e0fc5391e
Rodrigo Luiz <dolbrebr@gmail.com>
2022-01-29 19:35:55 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ searchBtn = body.querySelector('.search-box')
 modeSwitch = body.querySelector('.toggle-switch')
 modeText = body.querySelector('.mode-text')
 
+toggle.addEventListener('click', () => {
+  sidebar.classList.toggle('close')
+})
+
 modeSwitch.addEventListener('click', () => {
   body.classList.toggle('dark')
+
+  if (body.classList.contains('dark')) {
+    modeText.textContent = 'Light Mode'
+  } else {
+    modeText.textContent = 'Dark Mode'
+  }
 })
